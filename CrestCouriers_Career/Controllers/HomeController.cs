6bb0a0cebfa85d02d687d3d322e939963c1c11ad
mjzack424 <zack424@yahoo.com>
2019-11-27 19:01:45 +0330

    Career action result added
    توی این کامیت کد های سمت سرور صفحه career نوشته شد
    
    و همچنین تغییر کوچیکی در ویو ایجاد شد

--- CrestCouriers_Career/Controllers/HomeController.cs
+++ CrestCouriers_Career/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CrestCouriers_Career.Models;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace CrestCouriers_Career.Controllers
 {
@@ -37,6 +39,40 @@ namespace CrestCouriers_Career.Controllers
 
         public IActionResult Career()
         {
+
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Career(RegCareer career)
+        {
+
+
+            SqlConnection con = new SqlConnection(@"Data Source=daymond;Initial Catalog=Crest;Integrated Security=True;");
+            SqlCommand cmd = new SqlCommand("sp_Crest_Add", con);
+            con.Open();
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", career.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", career.LastName);
+            cmd.Parameters.AddWithValue("@Gender", career.Gender);
+            cmd.Parameters.AddWithValue("@Age", career.Age);
+            cmd.Parameters.AddWithValue("@Married", career.Married);
+            cmd.Parameters.AddWithValue("@HouseNumber", career.HouseNumber);
+            cmd.Parameters.AddWithValue("@RoadName", career.RoadName);
+            cmd.Parameters.AddWithValue("@City", career.City);
+            cmd.Parameters.AddWithValue("@PostCode", career.Married);
+            cmd.Parameters.AddWithValue("@DriverLicence", career.PostCode);
+            cmd.Parameters.AddWithValue("@Accident", career.Accident);
+            cmd.Parameters.AddWithValue("@DBS", career.DBS);
+            cmd.Parameters.AddWithValue("@PhoneNumber", career.PhoneNumber);
+            cmd.Parameters.AddWithValue("@Email", career.Email);
+            cmd.Parameters.AddWithValue("@UploadCV", career.UploadCV);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
 
             return View();
 
