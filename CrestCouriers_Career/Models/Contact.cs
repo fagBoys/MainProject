9af5a5d44e84d4898d5629b0d499927b6c779cbf
amirsahifi1374 <60890595+amirsahifi1374@users.noreply.github.com>
2020-03-31 15:58:25 +0430

    add contact model
    اضافه کردن کلاس contact

---
+++ CrestCouriers_Career/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrestCouriers_Career.Models
+{
+    public class Contact
+    {
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Website { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+
+
+
+    }
+}
