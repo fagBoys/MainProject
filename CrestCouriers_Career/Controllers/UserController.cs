5735ab1e9a26af98913605ad6ba897b0361f92c2
mjn110 <mjn220@gmail.com>
2020-07-09 01:34:38 +0430

    Some configuration in User
    در این کامیت جدید بعضی از خطاهای قسمت یوزر برطرف شده است

--- CrestCouriers_Career/Controllers/UserController.cs
+++ CrestCouriers_Career/Controllers/UserController.cs
@@ -248,13 +248,15 @@ namespace CrestCouriers_Career.Controllers
         }
         public IActionResult Dashboard()
         {
-            //if(HttpContext.Session.GetString("UserSession") == null)
-            //{
-            //    return new RedirectResult("/User/Login");
-            //}
-            { 
+            if (HttpContext.Session.GetString("UserSession") == null)
+            {
+                return new RedirectResult("/User/Login");
+            }
+            else
+            {
                 ViewData["Username"] = HttpContext.Session.GetString("UserSession");
             }
+
 
             string myurl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
             Dal connection = new Dal(myurl);
@@ -290,10 +292,11 @@ namespace CrestCouriers_Career.Controllers
 
         public IActionResult Edit(int id)
         {
-            //if (HttpContext.Session.GetString("UserSession") == null)
-            //{
-            //    return new RedirectResult("/User/Login");
-            //}
+            if (HttpContext.Session.GetString("UserSession") == null)
+            {
+                return new RedirectResult("/User/Login");
+            }
+            else
             {
                 ViewData["Username"] = HttpContext.Session.GetString("UserSession");
             }
@@ -326,10 +329,11 @@ namespace CrestCouriers_Career.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult EditOrder(Order order)
         {
-            //if (HttpContext.Session.GetString("UserSession") == null)
-            //{
-            //    return new RedirectResult("/User/Login");
-            //}
+            if (HttpContext.Session.GetString("UserSession") == null)
+            {
+                return new RedirectResult("/User/Login");
+            }
+            else
             {
                 ViewData["Username"] = HttpContext.Session.GetString("UserSession");
             }
@@ -359,6 +363,7 @@ namespace CrestCouriers_Career.Controllers
             {
                 return new RedirectResult("/User/Login");
             }
+            else
             {
                 ViewData["Username"] = HttpContext.Session.GetString("UserSession");
             }
@@ -375,6 +380,7 @@ namespace CrestCouriers_Career.Controllers
             {
                 return new RedirectResult("/User/Login");
             }
+            else
             {
                 ViewData["Username"] = HttpContext.Session.GetString("UserSession");
             }
@@ -430,6 +436,7 @@ namespace CrestCouriers_Career.Controllers
             {
                 return new RedirectResult("/User/Login");
             }
+            else
             {
                 ViewData["Username"] = HttpContext.Session.GetString("UserSession");
             }
@@ -462,6 +469,7 @@ namespace CrestCouriers_Career.Controllers
             {
                 return new RedirectResult("/User/Login");
             }
+            else
             {
                 ViewData["Username"] = HttpContext.Session.GetString("UserSession");
             }
