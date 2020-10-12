794755b5dd6050cd49731fc68213049af85a74a5
amirsahifi1374 <amir.h.sahifi@gmail.com>
2020-10-12 14:48:48 +0330

    bill creat
    مدل bill ساخته شد

---
+++ CrestCouriers_Career/Models/Bill.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrestCouriers_Career.Models
+{
+    public class Bill :DbContext
+
+    {
+        [Required]
+        public int BillID { get; set; }
+
+        
+        [MaxLength(50)]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public DateTime DownloadDate { get; set; }
+
+        [Required]
+        public byte File { get; set; }
+    }
+}
