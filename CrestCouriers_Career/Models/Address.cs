55dce9e77edbceecce905f9568a9fa5822e5edc3
amirsahifi1374 <amir.h.sahifi@gmail.com>
2020-11-20 16:32:09 +0330

    add model address
    مدل ادرس اضافه شد

---
+++ CrestCouriers_Career/Models/Address.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrestCouriers_Career.Models
+{
+    public class Address
+
+    {
+         [Key]
+        public int AddressId { get; set; }
+
+        [MaxLength(20)]
+        public string? Recipient { get; set; }
+
+        [MaxLength(20)]
+        public string? Company { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Street { get; set; }
+
+
+        [Required]
+        [MaxLength(20)]
+        public string Town { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Postcode { get; set; }
+
+    }
+}
