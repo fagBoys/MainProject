ec70f4d4633cd9debea0268a93a813938b858cc2
amir hosseyn <57629792+amirj666@users.noreply.github.com>
2021-05-29 20:06:34 +0430

    AddComment
    اصلاح کد

--- CrestCouriers_Career/Controllers/UserController.cs
+++ CrestCouriers_Career/Controllers/UserController.cs
@@ -21,6 +21,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System;
 
 namespace CrestCouriers_Career.Controllers
 {
@@ -549,15 +550,15 @@ namespace CrestCouriers_Career.Controllers
 
 
         [HttpPost]
-        public IActionResult AddComment(string Message, string AccountId, int ArticleId)
+        public IActionResult AddComment(string Message, int AccountId, int ArticleId)
         {
             CrestContext Context = new CrestContext();
             Comment comment = new Comment();
             comment.ArticleId = ArticleId;
             comment.Date = DateTime.Now;
-            comment.Text = Message;
+            comment.Message = Message;
             comment.AccountId = AccountId;
-            comment.Confirmation = "true";
+
 
             Context.Comment.Add(comment);
             Context.SaveChanges();
